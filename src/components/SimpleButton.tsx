3590a7dc9f4d9fad62314304cbb4e14f8308b15c
Parsa Nasirimehr <p.nasirimehr@gmail.com>
2021-03-26 21:18:12 +0430

    fixed issue with hitslop on SimpleButton.tsx

--- src/components/SimpleButton.tsx
+++ src/components/SimpleButton.tsx
@@ -4,13 +4,15 @@ import {Pressable, PressableProps, View, StyleSheet} from 'react-native';
 const SimpleButton = (props: PressableProps) => {
   return (
     <View style={[styles.container]}>
-      <Pressable {...props} hitSlop={40} />
+      <Pressable {...props} />
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
     height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 export default SimpleButton;
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -5,23 +5,15 @@ import {useNavigation} from '@react-navigation/native';
 
 import SimpleInput from '../components/SimpleInput';
 import SimpleButton from '../components/SimpleButton';
-import {Profile} from '../interfaces';
-import {login, PayLoad} from '../store/actions/login/login';
 import {colors} from '../config/colors';
 
-interface Props {
-  profile: Profile;
-  login: (payload: PayLoad) => void;
-}
-
-const SignUpPage = (props: Props) => {
+const SignUpPage = () => {
   const [userName, setUserName] = useState<string>('');
   const [passWord, setPassWord] = useState<string>('');
   const passwordRef = useRef<TextInput>(null);
   const navigation = useNavigation();
-  const {login} = props;
   const handlePress = () => {
-    login({username: userName, password: passWord});
+    console.log('successfull signup');
   };
   const goToLoginPage = () => {
     navigation.navigate('Login');
@@ -71,5 +63,4 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
 });
-const mapDispatchToProps = {login};
-export default connect(null, mapDispatchToProps)(SignUpPage);
+export default connect(null, null)(SignUpPage);
